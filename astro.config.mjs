6cd9217243ec5d6b2969faf0509e11dbbaa0669e
Atsuya Komatsu <126078195+Mr-Unchain@users.noreply.github.com>
2025-06-13 03:11:33 +0900

    feat: 新しいページとコンポーネントの追加、既存のコンポーネントの改善
    - お問い合わせページを追加し、Formspreeを使用してフォーム送信機能を実装。
    - プライバシーポリシーページを追加。
    - ユーザープロフィールページにスキルのパース機能を追加し、表示を改善。
    - ブログ詳細ページに目次機能を追加し、記事の表示を強化。
    - ヒーロースライドショーコンポーネントを更新し、画像の表示方法を改善。
    - サイドバーのスタイルを改善し、ユーザーインターフェースを向上。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com', // TODO: あなたのサイトのドメインに変更してください
   output: 'server', // 'hybrid'から'server'に修正しました！
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()],
+  image: {
+    domains: ["images.microcms-assets.io"],
+  },
 });
