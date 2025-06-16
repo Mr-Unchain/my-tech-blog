1fe8453ae4348453c5ebaf5aab7146ee425f75e2
Atsuya Komatsu <126078195+Mr-Unchain@users.noreply.github.com>
2025-06-17 01:28:49 +0900

    feat: Tailwind CSSの設定を更新し、コンポーネントのスタイルを改善
    - tailwind.config.mjsにカスタムカラーとフォントファミリーを追加。
    - ArticleCardコンポーネントで画像表示を改善し、カテゴリ表示を強化。
    - Footerコンポーネントのスタイルを更新し、レスポンシブデザインを向上。
    - Headerコンポーネントの背景色をカスタマイズし、ナビゲーションを改善。
    - HeroSlideshowコンポーネントのスタイルを調整し、インタラクションを向上。
    - Sidebarコンポーネントのスタイルを改善し、ユーザーインターフェースを向上。
    - BaseLayoutコンポーネントのスタイルを更新し、全体的なデザインを統一。

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 // tailwind.config.mjs
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,7 +9,18 @@ export default {
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-header": "#316C82",
+        "custom-button": "#FFD266",
+        "custom-text-bg": "#FFFFFF",
+        "custom-bg": "#F4F4F4",
+        "custom-heading": "#13B2AA",
+      },
+      fontFamily: {
+        sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
